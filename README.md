4b8e7df1c1f8e8418856742a8a248338dfe65842
shadmeoli <shadrack@shopzetu.com>
2024-04-06 15:18:13 +0300

    [fix] installation script update with license and readme for usage

---
+++ README.md
@@ -0,0 +1,14 @@
+## GO INSTALLER.
+
+I have created this script to help me install and setup **GO** on linux.
+The script basically does all that is needed to setup **GO**
+
+*Overview*
+The Go Installer Script automates the process of downloading and installing the Go programming language on Linux systems.
+
+## Installation
+To install Go using the installer script, run the following command in your terminal:
+
+```bash
+curl -sSL https://raw.githubusercontent.com/shadmeoli/go_installer/main/install_go.sh | bash
+```
--- install_go.sh
+++ install_go.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Function to add Go-related environment variables to shell configuration
 add_to_path() {
   # Prompt for shell type
   read -p "Enter your shell type (zsh/bash): " shell_type
@@ -23,19 +24,25 @@ add_to_path() {
   esac
 }
 
+# Download the latest Go release and install it
 latest_go_version=$(curl -s https://golang.org/VERSION?m=text)
 if wget -q "https://dl.google.com/go/$latest_go_version.linux-amd64.tar.gz"; then
+  # Extract the downloaded Go release to /usr/local
   sudo tar -C /usr/local -xzf "$latest_go_version.linux-amd64.tar.gz"
-  rm "$latest_go_version.linux-amd64.tar.gz"
+  rm "$latest_go_version.linux-amd64.tar.gz" # Remove the downloaded tar.gz file after extraction
+
+  # Add Go-related environment variables to the shell configuration
   add_to_path
   
+  # Clear the terminal before displaying output
+  clear
+
   # Color codes
   RED='\033[0;31m'
   GREEN='\033[0;32m'
   NC='\033[0m' # No Color
-  clear
-  
-  # ASCII art
+
+  # ASCII art of the Go logo
   echo -e "${GREEN}"
   echo "                                                                                                   "
   echo "                                                                                                   "
@@ -59,7 +66,10 @@ if wget -q "https://dl.google.com/go/$latest_go_version.linux-amd64.tar.gz"; the
   echo "                                                                                                   "
   echo -e "${NC}"
   
+  # Success message
   echo -e "${GREEN}Go has been successfully installed and configured.${NC}"
-else
+ else
+  # Error message if download fails
   echo -e "${RED}[!!] There was an error downloading the latest Go release.${NC}"
 fi
+
