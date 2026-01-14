872f5b91b3ae0250c571d2bac37a71089981dc28
shad <85517013+shadmeoli@users.noreply.github.com>
2026-01-14 10:07:01 +0300

    Fix Go installation script to correctly download latest version

--- install_go.sh
+++ install_go.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
 # Function to add Go-related environment variables to shell configuration
 add_to_path() {
   # Prompt for shell type
   shell_type=$SHELL
-
   # Check user input and proceed accordingly
   case "$shell_type" in
     "/usr/bin/zsh")
@@ -26,52 +24,52 @@ add_to_path() {
 
 # Function to install and set up Go
 install_go() {
-if wget -q "https://dl.google.com/go/$(curl 'https://golang.org/VERSION?m=text').linux-amd64.tar.gz"; then
-  # Extract the downloaded Go release to /usr/local
-  sudo tar -C /usr/local -xzf "$latest_go_version.linux-amd64.tar.gz"
-  rm "$latest_go_version.linux-amd64.tar.gz" # Remove the downloaded tar.gz file after extraction
-
-  # Add Go-related environment variables to the shell configuration
-  add_to_path
+  # Get the latest Go version
+  latest_go_version=$(curl -s 'https://golang.org/VERSION?m=text')
   
-  # Clear the terminal before displaying output
-  clear
-
-  # Color codes
-  RED='\033[0;31m'
-  GREEN='\033[0;32m'
-  NC='\033[0m' # No Color
-
-  # ASCII art of the Go logo
-  echo -e "${GREEN}"
-  echo "                                                                                                   "
-  echo "                                                                                                   "
-  echo "                                            +++++++++                   +++++++                     "
-  echo "                                       ++++++++++++++++++         ++++++++++++++++++                "
-  echo "                                     ++++++++++++++++++++++     +++++++++++++++++++++               "
-  echo "                                   +++++++++++++++++++++++++  +++++++++++++++++++++++++             "
-  echo "                 +++++++++++++++  ++++++++++++   +++++++++++ ++++++++++++   ++++++++++++            "
-  echo "                                 ++++++++++         +++     +++++++++          ++++++++++           "
-  echo "           +++++++++++++++++++  +++++++++      +++++++++++++++++++++            +++++++++           "
-  echo "                                +++++++++    ++++++++++++++++++++++              ++++++++           "
-  echo "                    +++++++++   ++++++++    +++++++++++++++++++++++             +++++++++           "
-  echo "                                +++++++++   +++++++++++++++++++++++             ++++++++            "
-  echo "                                ++++++++++         +++++++++++++++++          ++++++++++            "
-  echo "                                ++++++++++++    +++++++++++++++++++++++    ++++++++++++             "
-  echo "                                 +++++++++++++++++++++++++  +++++++++++++++++++++++++               "
-  echo "                                   +++++++++++++++++++++      ++++++++++++++++++++++                "
-  echo "                                     +++++++++++++++++         ++++++++++++++++++                   "
-  echo "                                        ++++++++++                 ++++++++++                       "
-  echo "                                                                                                   "
-  echo "                                                                                                   "
-  echo -e "${NC}"
-  
-  # Success message
-  echo -e "${GREEN}Go has been successfully installed and configured.${NC}"
- else
-  # Error message if download fails
-  echo -e "${RED}[!!] There was an error downloading the latest Go release.${NC}"
-fi
+  if wget -q "https://dl.google.com/go/${latest_go_version}.linux-amd64.tar.gz"; then
+    # Extract the downloaded Go release to /usr/local
+    sudo tar -C /usr/local -xzf "${latest_go_version}.linux-amd64.tar.gz"
+    rm "${latest_go_version}.linux-amd64.tar.gz" # Remove the downloaded tar.gz file after extraction
+    # Add Go-related environment variables to the shell configuration
+    add_to_path
+    
+    # Clear the terminal before displaying output
+    clear
+    # Color codes
+    RED='\033[0;31m'
+    GREEN='\033[0;32m'
+    NC='\033[0m' # No Color
+    # ASCII art of the Go logo
+    echo -e "${GREEN}"
+    echo "                                                                                                   "
+    echo "                                                                                                   "
+    echo "                                            +++++++++                   +++++++                     "
+    echo "                                       ++++++++++++++++++         ++++++++++++++++++                "
+    echo "                                     ++++++++++++++++++++++     +++++++++++++++++++++               "
+    echo "                                   +++++++++++++++++++++++++  +++++++++++++++++++++++++             "
+    echo "                 +++++++++++++++  ++++++++++++   +++++++++++ ++++++++++++   ++++++++++++            "
+    echo "                                 ++++++++++         +++     +++++++++          ++++++++++           "
+    echo "           +++++++++++++++++++  +++++++++      +++++++++++++++++++++            +++++++++           "
+    echo "                                +++++++++    ++++++++++++++++++++++              ++++++++           "
+    echo "                    +++++++++   ++++++++    +++++++++++++++++++++++             +++++++++           "
+    echo "                                +++++++++   +++++++++++++++++++++++             ++++++++            "
+    echo "                                ++++++++++         +++++++++++++++++          ++++++++++            "
+    echo "                                ++++++++++++    +++++++++++++++++++++++    ++++++++++++             "
+    echo "                                 +++++++++++++++++++++++++  +++++++++++++++++++++++++               "
+    echo "                                   +++++++++++++++++++++      ++++++++++++++++++++++                "
+    echo "                                     +++++++++++++++++         ++++++++++++++++++                   "
+    echo "                                        ++++++++++                 ++++++++++                       "
+    echo "                                                                                                   "
+    echo "                                                                                                   "
+    echo -e "${NC}"
+    
+    # Success message
+    echo -e "${GREEN}Go has been successfully installed and configured.${NC}"
+  else
+    # Error message if download fails
+    echo -e "${RED}[!!] There was an error downloading the latest Go release.${NC}"
+  fi
 }
 
 # Check for options
